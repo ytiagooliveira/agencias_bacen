00ca2948d8ef879e3d63aa8ff5d92843f537b0f5
ytiagooliveira <tiago.oliveira1921@outlook.com>
2024-12-03 20:25:25 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import "./styles.css"
+const Header = () =>{
+    return(
+        <header>
+            <h1 class="title">
+                Informes de Agências Bancárias Catalogadas pelo Bacen
+            </h1>
+        </header>
+    )
+}
+export default Header;
---
+++ src/pages/home/index.js
@@ -0,0 +1,11 @@
+import Header from '../../components/Header'
+function App(){
+    return(
+        
+        <div>
+            <Header />
+            teste de maluco
+        </div>
+    );
+}
+export default App;
